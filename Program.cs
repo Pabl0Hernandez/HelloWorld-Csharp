4dbc2654b6241f1468828b9dda582f9625ad874c
Sesi2 <Sesi2@SN-FWTB774>
2025-07-31 10:35:02 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var numero1 = 2;
+        var numero2 = 3;
+
+        var soma = numero1 + numero2;
+
+        Console.WriteLine("Bem Vindo ao Mundo de Programação C#");
+        Console.WriteLine("Falaaaa, Pablaoo!");
+
+        Console.WriteLine("A soma é igual a:", +soma);
+    }
+}
